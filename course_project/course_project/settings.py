ed8d4f06582067244d89cec77180cfae623fb855
Alexey Kurylev <chepe4pi@gmail.com>
2024-05-18 15:11:37 -0300

    update DB

--- course_project/course_project/settings.py
+++ course_project/course_project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'course_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db-new.sqlite3',
     }
 }
 
