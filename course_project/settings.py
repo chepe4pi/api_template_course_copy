2a09317caaff0aec15de7965d6bbfd278af85f2d
Alexey Kurylev <chepe4pi@gmail.com>
2024-06-08 23:34:52 -0300

    move

--- course_project/settings.py
+++ course_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'course_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
